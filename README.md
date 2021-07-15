0556a7879b8e1f8a9334d9114bd671f9e0fbfd65
杨冬青 <18210189817@qq.com>
2021-07-15 20:07:10 +0800

    :tada: init

---
+++ README.md
@@ -0,0 +1,4 @@
+# ui_template
+
+Flutter 学习
+> 参考，https://github.com/mitesh77/Best-Flutter-UI-Templates
---
+++ android/app/src/main/kotlin/com/yangdq/flutter/ui_template/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yangdq.flutter.ui_template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
